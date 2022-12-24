8e4a1483f6697a8f293bc44b05fb61989b47bc94
tkdrms0301 <tkdrms0301@naver.com>
2022-12-24 20:39:15 +0900

    file-upload-client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [files, setFiles] = useState([]);
+
+  const handleChange = (e) => {
+    if (e.target.files === null) return;
+
+    if (e.target.files[0]) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const handleClick = useCallback(async () => {
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await axios.post(
+      "http://localhost:4000/file/upload",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    if (res.status === 201) console.log(res.data);
+  }, [file]);
+
+  const getVideoList = async () => {
+    const path = "C:\\workspace\\github\\file-upload\\upload";
+    axios
+      .get(`http://localhost:4000/file/file-name?path=${path}`)
+      .then((res) => {
+        console.log(res);
+        setFiles(res.data.fileList);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input type={"file"} onChange={handleChange} />
+        <button onClick={handleClick}>업로드 요청</button>
+      </div>
+      <br></br>
+      <div>
+        <button onClick={getVideoList}>파일 리스트 요청</button>
+      </div>
+      <div>
+        {files.map((file, index) => {
+          return (
+            <div key={index}>
+              <h2>{file.name}</h2>
+              {console.log(file.name)}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
